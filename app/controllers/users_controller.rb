67bfc9c113c0becfb11f8d94889ce77be97e83cc
Eizer G. Relayson <egrelayson@up.edu.ph>
2018-11-17 02:06:01 +0800

    Updated users_controller to save user accounts (password still unencrypted and create_date null). Added show.html.erb as Success Prompt Page for user creation. Also updated CreateUsers class to limit username and password characters, as well as to reject null values.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id]) #save the user instance inside @user
+	end
+
 	def new
 
 	end
 
 	def create
-		render plain: params[:user].inspect #prints text field entries on screen
+		@user = User.new(user_params)
+
+		@user.save
+		redirect_to @user #Should redirect to Success Prompt Page / Thread create page
 	end
+
+	private
+		def user_params #For strong parameters
+			params.require(:user).permit(:username, :password)
+		end
+
 end
